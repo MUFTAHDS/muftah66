b8f954355f0dfa593165b29699bca0cc8ef57eaf
MUFTAHDSا <balraslymftah@gmail.com>
2021-11-22 16:34:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button, Badge, Breadcrumb, Navbar, NavDropdown, Nav, Container, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='test'>
+          <Button variant="primary">Primary</Button>{' '}
+          <Button variant="secondary">Secondary</Button>{' '}
+          <Button variant="success">Success</Button>{' '}
+          <Button variant="warning">Warning</Button>{' '}
+          <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+          <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+          <Button variant="link">Link</Button>
+        </div>
+
+
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+
+
+
+
+
+        <div>
+          <h1>
+            مفتاح عبد الله بالرأس علي <Badge bg="secondary">الأسم</Badge>
+          </h1>
+          <h2>
+            24سنة  <Badge bg="secondary">العمر </Badge>
+          </h2>
+          <h3>
+            25-12-1997 <Badge bg="secondary">مواليد</Badge>
+          </h3>
+          <h4>
+            مهندس مدني <Badge bg="secondary">التخصص</Badge>
+          </h4>
+          <h5>
+            0924345019  <Badge bg="secondary">رقم الهاتف </Badge>
+          </h5>
+          <h6>
+            أضغط على هذا الرابط  <Badge bg="secondary">للأستفسار </Badge>
+          </h6>
+        </div>
+        <Breadcrumb>
+          <Breadcrumb.Item href="#">Home</Breadcrumb.Item>
+          <Breadcrumb.Item href="https://getbootstrap.com/docs/4.0/components/breadcrumb/">
+            Library
+          </Breadcrumb.Item>
+          <Breadcrumb.Item active>Data</Breadcrumb.Item>
+        </Breadcrumb>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+
       </header>
-    </div>
+
+    </div >
   );
 }
 
